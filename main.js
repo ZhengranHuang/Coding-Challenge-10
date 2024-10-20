a3c9bfd7a91e473ebc803ff01747528fa822d4bf
ZhengranHuang <Zhengran@usf.edu>
2024-10-20 17:47:06 -0400

    final try

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ Small: 4,
 Medium: 5,
 Big: 6
 };
-const Smallprice = document.getElementById("small")
-const Mediumprice = document.getElementById("medium")
-const Bigprice = document.getElementById("big")
+const Smallprice = document.getElementById("small");
+const Mediumprice = document.getElementById("medium");
+const Bigprice = document.getElementById("big");
 Smallprice.addEventListener('click',function(){
 console.log(`Price is $${price.Small}`)
 });
@@ -18,4 +18,10 @@ console.log(`Price is $${price.Medium}`)
 });
 Bigprice.addEventListener('click',function(){
 console.log(`Price is $${price.Big}`)
+});
+//Task3:so hard:(
+//Task4:Create a Checkout Event Add an event listener to the purchase button
+const Purchasebutton = document.getElementById("purchase");
+Purchasebutton.addEventListener('click',function(){
+console.log(`this assignment is so hard`)
 });
